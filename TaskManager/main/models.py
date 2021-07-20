50ab1f73e15093f6973e3d1825149c240943b52b
lzn747 <lzn747@gmail.com>
2021-07-20 16:38:17 +0300

    Models created. Database initialized.

--- TaskManager/main/models.py
+++ TaskManager/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Task Title', max_length=50)
+    task = models.TextField('Task Body')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'My Tasks'
