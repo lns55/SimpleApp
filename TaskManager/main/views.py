3f703d67687336bdec63349f5d22b40d29380903
lzn747 <lzn747@gmail.com>
2021-07-20 17:11:31 +0300

    Simple operations with data implemented.

--- TaskManager/main/views.py
+++ TaskManager/main/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TaskForm
 from .models import Task
 
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html', {'title': 'Home Page', 'tasks': tasks})
 
 
 def about(request):
     return render(request, 'main/about.html')
 
+
+def add(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form is not valid'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/add.html', context)
